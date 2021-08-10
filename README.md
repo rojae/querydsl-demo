d0a18e59afed5305b372e247292cfc83920c12bb
rojae <Jae.Thon96@gmail.com>
2021-08-10 23:37:53 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Querydsl Demo Project
+
+- jdk8
+- springboot 2.5.3
+- gradle
---
+++ src/test/java/kr/rojae/querydsl/ApplicationTest.java
@@ -0,0 +1,43 @@
+package kr.rojae.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.rojae.querydsl.entity.Hello;
+import kr.rojae.querydsl.entity.QHello;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional(readOnly = true)
+@Slf4j
+class ApplicationTest {
+
+	@Autowired
+	private EntityManager em;
+
+	@Test
+	@Transactional
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query.selectFrom(qHello).fetchOne();
+
+		assertThat(hello).isEqualTo(result);
+		assertThat(hello.getId()).isEqualTo(result.getId());
+
+		log.info("hello : " + hello.getId());
+		log.info("result : " + result.getId());
+	}
+
+}
